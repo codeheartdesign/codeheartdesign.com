e4cf1feb6aecb210578c9a628aef7dcdba2e5c51
Dominik Wilkowski <Hi@Dominik-Wilkowski.com>
2018-11-13 18:38:36 +1100

    updated unfurl image and cache busting
    Signed-off-by: Dominik Wilkowski <Hi@Dominik-Wilkowski.com>

--- code/layout/head.js
+++ code/layout/head.js
@@ -35,14 +35,14 @@ const Head = ({ _ID, _relativeURL, pagetitle }) => {
 			<meta property="og:title" content="Code Heart Design Conference Sydney"/>
 			<meta property="og:site_name" content="Code Heart Design Conference Sydney"/>
 			<meta property="og:url" content="https://codeheartdesign.com/"/>
-			<meta property="og:image" content="https://codeheartdesign.com/assets/img/og-image.jpg?v2"/>
+			<meta property="og:image" content="https://codeheartdesign.com/assets/img/og-image.jpg?v3"/>
 			<meta property="og:image:height" content="1257"/>
 			<meta property="og:image:width" content="2400"/>
 			<meta name="twitter:card" content="summary_large_image"/>
 			<meta name="twitter:title" content="Code Heart Design Conference Sydney"/>
 			<meta name="twitter:description" content="A mini-conf bringing together designers, design system lovers and front-end developers."/>
 			<meta name="twitter:name" content="Code Heart Design Conference Sydney"/>
-			<meta name="twitter:image" content="https://codeheartdesign.com/assets/img/og-image.jpg?v2"/>
+			<meta name="twitter:image" content="https://codeheartdesign.com/assets/img/og-image.jpg?v3"/>
 			<style dangerouslySetInnerHTML={{ __html: Styles() }} />
 			<link href="https://fonts.googleapis.com/css?family=Lora:400,400i,700i" rel="stylesheet"/>
 			<link rel="stylesheet" href={_relativeURL(`/assets/css/site.min.css`, _ID)}/>
